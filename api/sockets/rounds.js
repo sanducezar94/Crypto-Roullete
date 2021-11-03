021bcd2af1e8cfc52edd55b432bbd1e68f122401
Sandu Cezar <hopeslicer@gmail.com>
2021-11-03 19:01:05 +0200

    Update rounds.js

--- api/sockets/rounds.js
+++ api/sockets/rounds.js
@@ -1,8 +1,26 @@
 const { User, Bet, Round } = require("../models");
+const ERROR_MESSAGES = require("../constants/messages.js");
+const _roundRepo = require("../repository/rounds.js");
+const validate = require("../services/validator.js");
 
 const roundsIO = (socket) => {
-  socket.on('start_round', async (args) => {
+  socket.on('reward_players', async (args) => {
+    const lastRound = await _roundRepo.getLastRound();
+    const bets = await Bet.findAll({
+      where: { roundId: lastRound.id }
+    });
 
+    //this will be handled on the smart contract once deployed
+    for(let i = 0; i < bets.length; i++){
+      if(bets[i].color === lastRound.result){
+        const user = User.findOne({
+          where: { id: bet[i].userId }
+        });
+        require(user, ERROR_MESSAGES.INVALID_REQUEST);
+        const newBalance = user.balance + (bets[i].amount * lastRound.resultMultiplier);
+        await User.update({ balance: newBalance }, {where: {id: user.id}});
+      }
+    }
   });
 };
 
